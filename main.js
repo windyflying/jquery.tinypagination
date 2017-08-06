d8347cb296ca11c69864778e18d89c0349f240bc
windyflying <xss_ci@163.com>
2017-08-06 23:03:49 +0800

    add comments

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ requirejs.config({
   }
 });
 
-require(['jquery.page', 'jquery'], function () {
-  console.log('arguments.length = %d', arguments.length)
-  var jQ = arguments[1];
-  var $pagination = jQ('#pagination');
+// 注意，这里故意把'jquery'作为第二个参数，这并不会导致运行时错误
+// 因为模块加载器会分析'jquery'和'jquery.page'的依赖关系。有一点
+// 很重要，'jquery.page'模块的定义方式（查看js/app/jquery.page.js文件），
+// 即define(['jquery'], factory)
+// 说明了jquery.page模块依赖jquery，因此factory函数会等到jquery模块加载
+// 完成后才会执行。
+require(['jquery.page', 'jquery'], function (jpage, $) {
+  // jquery和jquery.page模块加载完成才会执行这里
+  var $pagination = $('#pagination');
 
   $pagination.tinyPagination({
     pageCount: 999,
